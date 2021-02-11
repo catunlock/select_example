68a918a40f1c96b386958fa7ac521a89656c2d3e
Alberto López Sánchez <alopez@cells.es>
2021-02-11 11:32:17 +0100

    try to use pyserial

--- super_serial.py
+++ super_serial.py
@@ -1,20 +1,22 @@
 import selectors
-from socket import socket
+import serial
 import threading
+import os
 
 
 class SuperSerial:
     def __init__(self) -> None:
         self.sel = selectors.DefaultSelector()
-        self.sock = socket()
-        self.sock.connect(("localhost", 4004))
-        self.sock.setblocking(False)
-        self.sel.register(self.sock, selectors.EVENT_READ, self._read)
+
+        self._com = serial.serial_for_url("/tmp/serial1")
+        self.sel.register(self._com, selectors.EVENT_READ, self._read)
 
         self.lines = []
         self.read_lock = threading.Lock()
         self.read_buffer = bytearray()
         self.endline = b"\n"
+
+        self.running = False
 
     def run(self):
         while self.running:
@@ -25,25 +27,26 @@ class SuperSerial:
             
     def start(self):
         self.thread = threading.Thread(target=self.run)
-        self.thread.daemon = True
+        # self.thread.daemon = True
         self.running = True
         self.thread.start()
 
     def stop(self):
         self.running = False
 
-    def _read(self, conn: socket, event: int):
-        data = conn.recv(1000)  # Should be ready
+    def _read(self, com, event: int):
+        print("COOOM: {}".format(com))
+        data = os.read(self._com.fileno(), 1024)
         if data:
             with self.read_lock:
                 self.read_buffer.extend(data)
         else:
-            print('closing', conn)
-            self.sel.unregister(conn)
-            conn.close()
+            print('closing', self._com)
+            self.sel.unregister(self._com.fileno())
+            self._com.close()
 
     def write(self, buf: bytes):
-        self.sock.sendall(buf)
+        self._com.write(buf)
 
     def _get_read_buffer(self):
         with self.read_lock:
@@ -51,11 +54,10 @@ class SuperSerial:
             self.read_buffer = bytearray()
         return buff
 
-    def read(self) -> str:
-        buff = self._get_read_buffer()
-        return buff.decode("UTF-8")
+    def read(self) -> bytearray:
+        return self._get_read_buffer()
 
-    def readline(self) -> str:
+    def readline(self) -> bytearray:
         buff = self._get_read_buffer()
         line, sep, remaining = buff.partition(self.endline)
         with self.read_lock:
